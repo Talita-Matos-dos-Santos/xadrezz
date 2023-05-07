a9c328e7219ab0b521c955b28ce7a48c056d24d3
Talita Matos <talitagvmatos@gmail.com>
2023-05-07 16:32:02 -0300

    criando a class PosicaoXadrez

--- xadrezConsole/Program.cs
+++ xadrezConsole/Program.cs
@@ -8,22 +8,9 @@ namespace xadrezConsole
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 7));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-
-                Tela.imprimirTabuleiro(tab);
-
-            }
-
-            catch (TabuleiroException te)
-            {
-                Console.WriteLine(te.Message);
-            }
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos.toPosicao());
+            Console.WriteLine(pos);
         }
     }
 }
---
+++ xadrezConsole/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,27 @@
+using tabuleiro;
+namespace xadrez;
+
+class PosicaoXadrez
+{
+    //essa class serve só pra auxiliar a pensar no contexto do xadrez, pois as posicoes do xadrez ficam escrito diferente das posicoes da matriz do C#.
+    //so que internamente eu quero converter a posicao aqui do xadrez para uma posicao interna la da matriz -> A,8 terá que ser 0,0
+    public char coluna { get; set; }
+    public int linha { get; set; }
+
+    public PosicaoXadrez(char coluna, int linha)
+    {
+        this.coluna = coluna;
+        this.linha = linha;
+    }
+
+    public Posicao toPosicao()
+    {
+        //obs: internamente o caractere 'a' é um numero inteiro, ai se for 'a'- 'a' vai dar 0, se for 'b' - 'a', considerando que b é a proxima letra dps de a, ent b - a da 1. da pra pensar assim: se eu to na letra d, qnts casas eu ando de a ate d? 3, entao coluna 3
+        return new Posicao(8 - linha, coluna - 'a');
+    }
+
+    public override string ToString()
+    {
+        return coluna + ", " + linha;
+    }
+}
